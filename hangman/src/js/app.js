105879c126b1864db1e7651e0fd675306b906a97
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-01-15 20:02:45 +0300

    feat: add generating keyboard function

--- hangman/src/js/app.js
+++ hangman/src/js/app.js
@@ -1,1 +1,6 @@
 import '../scss/style.scss';
+import generateHtml from './files/generate-html';
+
+window.addEventListener('load', () => {
+  generateHtml();
+});
---
+++ hangman/src/js/files/generate-html.js
@@ -0,0 +1,50 @@
+import { dataCodes, labels } from './keys-labels';
+
+const generateHtml = () => {
+  const keyboard = document.createElement('div');
+  keyboard.classList.add('game__keyboard');
+
+  const keys = [];
+
+  for (let i = 0; i < 26; i++) {
+    const key = document.createElement('div');
+    key.classList.add('keyboard__key');
+    key.setAttribute('data-code', dataCodes[i]);
+    key.textContent = labels[i];
+    keys.push(key);
+  }
+
+  for (let i = 0; i < 3; i += 1) {
+    const row = document.createElement('div');
+    row.classList.add('keyboard__row');
+
+    switch (i) {
+      case 0: {
+        for (let j = 0; j < 9; j += 1) {
+          row.append(keys[j]);
+        }
+        break;
+      }
+      case 1: {
+        for (let j = 9; j < 18; j += 1) {
+          row.append(keys[j]);
+        }
+        break;
+      }
+      case 2: {
+        for (let j = 18; j < 26; j += 1) {
+          row.append(keys[j]);
+        }
+        break;
+      }
+      default:
+        break;
+    }
+
+    keyboard.append(row);
+  }
+
+  document.querySelectorAll('.game__column')[1].appendChild(keyboard);
+};
+
+export default generateHtml;
