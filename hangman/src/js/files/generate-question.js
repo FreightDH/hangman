b46a179e1deb7c0c7ec014fd666302f9d250c25d
Max Zolotkov <maxim.zolotkov@yandex.ru>
2024-01-16 14:45:57 +0300

    feat: add generateQuestion function

---
+++ hangman/src/js/files/generate-question.js
@@ -0,0 +1,44 @@
+import { createElement, getRandomNumber } from './helpers.js';
+import questions from './questions.js';
+
+const generateQuestion = () => {
+  const wordWrapper = document.querySelector('.game__word');
+  const hint = document.querySelector('.game__hint');
+  const incorrectCounter = document.querySelector('.game__counter span');
+  const prevWord = window.prevWord || '';
+  const { word, question } = getNewWord(prevWord);
+
+  wordWrapper.innerHTML = '';
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = createElement(
+      'div',
+      'word__letter',
+      '<div class="letter__text"></div>\n<div class="letter__underscore"></div>'
+    );
+
+    wordWrapper.appendChild(letter);
+  }
+
+  hint.textContent = `Hint: ${question}`;
+  incorrectCounter.textContent = `0 / ${word.length}`;
+
+  window.answerLength = word.length;
+  window.inputLettersCount = 0;
+};
+
+const getNewWord = (prevWord) => {
+  let randIdx = 0;
+
+  do {
+    randIdx = getRandomNumber(0, Object.keys(questions).length - 1);
+  } while (Object.keys(questions)[randIdx] === prevWord);
+
+  const word = Object.keys(questions)[randIdx];
+  const question = questions[word];
+
+  window.prevWord = word;
+  return { word, question };
+};
+
+export default generateQuestion;
