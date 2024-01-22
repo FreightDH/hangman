fbe1b3cae608fb2d1f631c557f10ed93fd785af1
FreightDH <maxim.zolotkov@yandex.ru>
2024-01-22 18:10:24 +0300

    feat: add generateManIcon function

--- hangman/src/js/files/generate-html.js
+++ hangman/src/js/files/generate-html.js
@@ -1,6 +1,15 @@
+import { createElement } from './helpers';
 import { dataCodes, labels } from './info/keys-labels';
 
 const generateHtml = () => {
+  const keyboard = generateKeyboard();
+  const manIcon = generateManIcon();
+
+  document.querySelectorAll('.game__column')[1].appendChild(keyboard);
+  document.querySelector('.gallows__man').appendChild(manIcon);
+};
+
+const generateKeyboard = () => {
   const keyboard = document.createElement('div');
   keyboard.classList.add('game__keyboard');
 
@@ -44,7 +53,24 @@ const generateHtml = () => {
     keyboard.append(row);
   }
 
-  document.querySelectorAll('.game__column')[1].appendChild(keyboard);
+  return keyboard;
+};
+
+const generateManIcon = () => {
+  const order = ['head', 'body', 'hand', 'hand hand--right', 'leg', 'leg leg--right'];
+  const manWrapper = createElement('div', 'man__wrapper');
+
+  order.map((item) => {
+    const element = createElement(
+      'div',
+      `man__${item}`,
+      `<img src="img/man/${item.split(' ')[1] || item.split(' ')[0]}.svg" alt="${item.split(' ')[0]}-icon" />`,
+    );
+    manWrapper.appendChild(element);
+    return element;
+  });
+
+  return manWrapper;
 };
 
 export default generateHtml;
